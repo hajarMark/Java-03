7272e711f98801a9c209de225ec0cde348fbd9d6
hajarMark <66554999+hajarMark@users.noreply.github.com>
2021-03-30 14:20:23 +0200

    Add files via upload

---
+++ Declaration.java
@@ -0,0 +1,15 @@
+package Variables;
+
+public class Declaration {
+
+	public static void main(String[] args) {
+		String title="Indiana Jones et la dernière croisade";
+		boolean wached=false;
+		int year=1989;
+		double rating=8.2;
+		System.out.println(title);
+		System.out.println(wached);
+		System.out.println(year);
+    	System.out.println(rating);
+	}
+}
